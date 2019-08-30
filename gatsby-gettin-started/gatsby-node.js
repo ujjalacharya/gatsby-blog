6950dab9c3ad81e456aa4799d0dd0f833fe5485d
Ujjal Acharya <acharyaujjal1@gmail.com>
2019-08-30 10:07:18 +0000

    render  image on the page

--- gatsby-gettin-started/gatsby-node.js
+++ gatsby-gettin-started/gatsby-node.js
@@ -1,43 +1,27 @@
 const path = require("path");
 
-exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField } = actions
-
-    if (node.internal.type == "MarkdownRemark") {
-        const slug = path.basename(node.fileAbsolutePath,  ".md");
-
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
-}
-
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const blogTemplate = path.resolve('./src/templates/blog.js');
 
   const res = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-          }
-        }
+   query{
+  allContentfulBlog{
+    edges{
+      node{
+        slug
       }
     }
+  }
+}
   `)
 
-  res.data.allMarkdownRemark.edges.map(edge => {
+  res.data.allContentfulBlog.edges.map(edge => {
      return createPage({
           component: blogTemplate,
-          path: `/blog/${edge.node.fields.slug}`,
+          path: `/blog/${edge.node.slug}`,
           context: {
-              slug: edge.node.fields.slug
+              slug: edge.node.slug
           }
       })
   })
--- gatsby-gettin-started/src/templates/blog.js
+++ gatsby-gettin-started/src/templates/blog.js
@@ -2,27 +2,40 @@ import React from "react";
 
 import {graphql} from "gatsby";
 
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
 import Layout from "../components/Layout";
 
-export const query = graphql`
-        query($slug: String){
-         markdownRemark(fields: {slug: {eq: $slug}}){
-            frontmatter{
-                title
-                date
-    }
-    html
+export const data = graphql`
+    query($slug: String){
+        contentfulBlog(slug: {eq: $slug}){
+            title
+            published(formatString: "MMMM Do YYYY")
+            body{
+                json
+            }
   }
 }
-    `
+`;
 
 const Blog = (props) => {
 
+    const options = {
+        renderNode: {
+            "embedded-asset-block": (node) => {
+                const alt = node.data.target.fields.title['en-US']
+                const url = node.data.target.fields.file['en-US'].url
+
+                return <img src={url} alt={alt} />
+            }
+        }
+    }
+
     return (
         <Layout>
-            <h2>{props.data.markdownRemark.frontmatter.title}</h2>
-            <p>{props.data.markdownRemark.frontmatter.date}</p>
-            <div  dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}}></div>
+                 <h2>{props.data.contentfulBlog.title}</h2>
+                 <p>{props.data.contentfulBlog.published}</p>
+                 {documentToReactComponents(props.data.contentfulBlog.body.json, options)}
         </Layout>
     )
 }
